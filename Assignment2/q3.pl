41825b65efbcfded61a0240b0e0f005fef088dda
Apurva N. Saraogi <apurva9@iitg.ernet.in>
2019-10-24 19:37:28 +0530

    Q3 complete undirected case remaining

--- Assignment2/q3.pl
+++ Assignment2/q3.pl
@@ -1,5 +1,6 @@
 
 % Weights of all the edges/paths from X to Y containing W weight.
+
 weights('G1','G5',4).
 weights('G2','G5',6).
 weights('G3','G5',8).
@@ -45,7 +46,6 @@ start('G1').
 start('G2').
 start('G3').
 start('G4').
-start('G5').
 
 end('G17').
 
@@ -55,8 +55,16 @@ edge(X,Y):- weights(X,Y,_).
 undirected_edge(X,Y):- weights(X,Y,_).
 undirected_edge(X,Y):- weights(Y,X,_).
 
+% Global Variables for storing the best weight and best paths.
+:-dynamic(best_weight/1).
+:-dynamic(best_paths/1).
+best_weight(999999).
+
+
+% Function for checking if a given list is empty or not.
 is_empty([]).
 
+% check_path function sees in the given list if the edge is valid then recursibely move to the next edge otherwise break. If the last element is the ending vertex then only return true.
 check_path(List):-
 	[H|T] = List,
 	is_empty(T),
@@ -68,25 +76,102 @@ check_path(List):-
 	[_|T] = List,
 	check_path(T).
 
+% valid Function is the one that is called by the user. It will check first if the start of the list is one of the given starting nodes and then call check_path. If the given input is not a valid path then it will go to the second function of valid.
 valid(List):-
 	[H|_] = List,
 	start(H),
 	check_path(List),
-	write("Path Found"),
+	write("Path Found").
+
+valid(_):-
+	write("Path Not Found").
+
+% print_path is used to print a given list containing vertices with arrows in middle of them.
+print_path(_,[]).
+
+print_path(Prefix,[H|T]):-
+	is_empty(T),
+	write(Prefix),
+	write(H),
+	nl.	
+
+print_path(Prefix,[H|T]):-
+	\+ is_empty(T),
+	format("~w~w -> ",[Prefix,H]),
+	print_path("",T).
+
+% Given Y as the maximum Sum observed till now and Sum is the current Path's sum.
+% 	If Y > Sum update Y to sum and delete all previous paths store the current path.
+%	If Y == Sum add current Path to best_paths.
+
+update(Y,Sum,Path):-
+	Y > Sum,
+	retract(best_weight(Y)),
+	asserta(best_weight(Sum)),
+	retractall(best_paths(_)),
+	assertz(best_paths(Path)).
+
+update(Y,Sum,Path):-
+	Y =:= Sum,
+	assertz(best_paths(Path)).
+
+
+% dfs_util is the utility function called by dfs function.
+% 	If current Path's sum is greater than best path sum, we can drop it and dont need to go ahead in this path's direction.
+% 	If we have reached the end then call the update function.
+% 	Else look at all edges from current node and call dfs on them.
+
+dfs_util(_,Sum,_):-
+	best_weight(Y),
+	Sum >= Y.
+
+dfs_util(C,Sum,Path):-
+	end(C),
+	append(Path,[C],NewPath),	
+	best_weight(Y),
+	update(Y,Sum,NewPath).
+
+dfs_util(C,Sum,Path):-
+	append(Path,[C],NewPath),
+	edge(C, R),
+	weights(C,R,Y),
+	NewSum is Sum+Y,	
+	dfs_util(R,NewSum,NewPath),
+	fail.
+
+% start dfs function from all possible starting locations. 
+dfs:-
+	start(C),	
+	dfs_util(C,0,[]),
+	fail.
+
+% print_paths prints all of the best paths using best_paths function.
+print_paths:-
+	best_paths(Y),
+	print_path("Path: ",Y),
+	fail.
+
+% print_weight checks if there is any path. If there is it prints the result.  
+print_weight(Z):-
+	Z =:= 999999,
+	print("No paths found from given start to end").
+
+print_weight(Z):-
+	format("Weight of Path: ~w ~n",[Z]).
+
+% precheck_database alerts the user if there is any negative distance.
+precheck_database:-
+	weights(_,_,X) , X < 0,
+	print("***Negative Weights Found***"),
 	nl.
 
-valid(List):-
-	write("Path Not Found"),
-	nl.
-
-
-
-
-
-
-
-
-
-
-
+% optimal function is called by the user which will first check the database, then do dfs, then print the results.
+optimal:-
+	\+ precheck_database,
+	\+ dfs,
+	write("The following paths are the shortest:"),
+	nl,
+	\+ print_paths,
+	best_weight(Z),
+	print_weight(Z).
 
