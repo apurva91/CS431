9decefc5f84400a56b5a8f094ae2aa7cf59297bd
Apurva N. Saraogi <apurva9@iitg.ernet.in>
2019-11-20 16:52:21 +0530

    q1 and q2 complete with commenting

--- Assignment3/q1.hs
+++ Assignment3/q1.hs
@@ -1,6 +1,7 @@
 import Data.List
 import System.IO
 
+
 -- Declaring signature of function m
 m :: [[Int]] -> Int
 
@@ -10,6 +11,18 @@ m x = product [sum y | y <- x]
 
 -- Declaring signature of function greatest
 greatest :: (a -> Int) -> [a] -> a
-
 greatest func [] = error "List is Empty. Please add some elements."
 greatest func list = list !! (snd (maximum [ (func x, i) | (i,x) <- zip [0..] list ]))
+
+-- Definition for the data type List
+data List a = Empty | Cons a (List a) deriving Show
+
+-- Conversion to list
+toList :: [a] -> List a
+toList ([]) = Empty
+toList (x:xs) = Cons x (toList(xs))
+
+-- Conversion from list
+toHaskellList :: List a -> [a]
+toHaskellList(Empty) = []
+toHaskellList(Cons a b) = a : toHaskellList(b)
--- Assignment3/q2.hs
+++ Assignment3/q2.hs
@@ -1,13 +1,16 @@
 import Data.List
-import Data.String
 
+-- Generate All Possible Substrings Also provide sorted version of them
 allSubs :: String -> [(String,String)] 
 allSubs x = [ (z, sort z) | i <- [0..length(x)-1], j<-[1..length(x)-i], z<-[(take j (drop i x))] ]
 
+-- Group set of given strings a/c to second
 groupSubsets :: [(String,String)] -> [[(String, String)]]
 groupSubsets x = groupBy (\(_,a) (_,b) -> a==b) (sortOn snd x)
 
-get :: String -> String -> (Int, [(String,String)])
-get s1 s2 = (length z, z)
-    where subs = groupSubsets $ allSubs $ s1++s2
+-- Main Function - Group subsets return all combinations
+compute :: [String] -> (Int, [(String,String)])
+compute s | length(s)/=2 = (0,[("Function accepts a list with only two `String` or `[Char]`.","")]) 
+compute s = (length z, z)
+    where subs = groupSubsets $ allSubs $ s!!0++s!!1
           z = [ ( fst $ x!!i , fst $ x!!j ) | x <- subs, i <- [0..length(x)-1], j <- [i+1..length(x)-1] ]
